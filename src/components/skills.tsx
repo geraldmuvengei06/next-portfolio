0ffacc208dc90f27ca1b139b428bfb2f854a6e47
Gerald <geraldmuvengei06@gmail.com>
2023-07-03 13:22:52 +0300

    Update skills.tsx

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -8,7 +8,7 @@ export default function Skills() {
             <div className="sm:px-16 ">
                 <div className="p-4">
                     <h2 className=" text-4xl font-bold mb-4">Skills<span className="text-primary">_</span></h2>
-                    <p>Here are some of the skills that I've obtained and worked with over my three year software development experience.</p>
+                    <p>Here are some of the skills that I have obtained and worked with over my three year software development experience.</p>
                     <div className="skills flex flex-wrap my-4">
                         {
                             skills.map((skill, index) => {
@@ -22,7 +22,7 @@ export default function Skills() {
                                     //     </div>
                                     // </div>
 
-                                    <div className="card p-2 w-32 h-32 bg-opacity-70 backdrop-blur shadow-sm bg-white dark:bg-dark m-2  border-transparent hover:ring-1 hover:ring-slate-700">
+                                    <div key={index + 'skills'}  className="card p-2 w-32 h-32 bg-opacity-70 backdrop-blur shadow-sm bg-white dark:bg-dark m-2  border-transparent hover:ring-1 hover:ring-slate-700">
                                         <div className="flex flex-col items-center justify-around ">
                                             <img className="w-20 h-20 mb-2 object-contain" src={skill?.icon} alt={skill?.title} />
                                             <span className="text-sm text-gray-500 dark:text-gray-400">{skill?.title}</span>
